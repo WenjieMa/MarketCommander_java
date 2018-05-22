eebff47c1e1071a9adeb46d6000f76f000c1fa07
marven <754825416@qq.com>
2018-05-22 19:44:16 +0800

    ''

--- src/main/java/com/dao/JPAInterface/IRecordRefundDAO.java
+++
@@ -1,8 +0,0 @@
-package com.dao.JPAInterface;
-
-import com.pojo.RecordRefund;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface IRecordRefundDAO extends JpaRepository<RecordRefund, Long>,JpaSpecificationExecutor<RecordRefund> {
-}
--- src/main/java/com/dao/daoimpl/RecordRefundDAO.java
+++ src/main/java/com/dao/daoimpl/RecordRefundDAO.java
@@ -1,7 +1,4 @@
 package com.dao.daoimpl;
 
-import com.pojo.RecordRefund;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 public class RecordRefundDAO extends BaseDao{
 }
