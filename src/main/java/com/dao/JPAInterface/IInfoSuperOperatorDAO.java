8e8a451b17960aa47c3cc52de66ae5a1bcbcf84f
marven <754825416@qq.com>
2018-05-22 20:11:41 +0800

    ''

--- src/main/java/com/dao/JPAInterface/IInfoSuperOperatorDAO.java
+++
@@ -1,8 +0,0 @@
-package com.dao.JPAInterface;
-
-import com.pojo.InfoSuperOperator;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface IInfoSuperOperatorDAO extends JpaRepository<InfoSuperOperator, Long>,JpaSpecificationExecutor<InfoSuperOperator> {
-}
